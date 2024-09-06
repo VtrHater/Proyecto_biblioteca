81c8b28afd76646f820778cd59d0f66c500c98ad
VtrHater <ealvarez@usm.cl>
2024-09-06 17:27:00 -0400

    Creacion cuentas V0.1

--- Trabajo_Biblioteca/Cuentas/views.py
+++ Trabajo_Biblioteca/Cuentas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+
+def registrar(request):
+    titulo = "Hola gente"
+    return render(request, 'base.html',{
+        "titulo": titulo
+    })
+     
+
+
+
 # Create your views here.
