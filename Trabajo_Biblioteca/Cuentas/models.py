ce99461e9d07e69a6efc12a83351d77df5a8f926
VtrHater <ealvarez@usm.cl>
2024-09-21 11:53:57 -0300

    Incorporación de la base de datos V1.0

--- Trabajo_Biblioteca/Cuentas/models.py
+++ Trabajo_Biblioteca/Cuentas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Personal(models.Model):
+    name = models.CharField(max_length=100)
+    sector = models.CharField(max_length=60)
+
+class Solicitudes(models.Model):
+    documento = models.CharField(max_length=100)
+    departamento = models.CharField(max_length=60)
+    nota = models.TextField(blank=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    autor = models.CharField(max_length=60)
+    
